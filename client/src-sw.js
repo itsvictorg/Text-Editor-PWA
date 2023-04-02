e03aea301d57333e02ee7ae2d0e02d5ba1f7fd9d
unknown <itsvictorg@gmail.com>
2023-04-02 17:12:59 -0500

    working on webpack error

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,11 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({req}) => req.destination === 'image', new CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new CacheableResponsePlugin({ statuses: [0, 200] }),
+    new ExpirationPlugin({ maxAgeSeconds: 30 * 24 * 60 * 60
+    })
+  ]}
+));

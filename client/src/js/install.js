b1b8722e333687332343b6d01a4f156fe15460da
unknown <itsvictorg@gmail.com>
2023-04-02 16:51:07 -0500

    added event handlers for installation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,41 @@
 const butInstall = document.getElementById('buttonInstall');
 
+butInstallHandler = async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    butInstall.classList.toggle('hidden', true);
+};
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+
+window.deferredPrompt = event;
+
+	// Remove the hidden class from the button.
+	butInstall.classList.toggle('hidden', false);
+    
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstallHandler();
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed');
+    window.deferredPrompt = null;
+});
